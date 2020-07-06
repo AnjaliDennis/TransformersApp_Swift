efbd3811cbec9ed63a3e31145b8c515ec04cd400
Anjali Dennis <anjalidennis13@gmail.com>
2020-07-06 15:19:53 +0100

    Initial Commit

---
+++ TransformersAppSwiftTests/TransformersAppSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  TransformersAppSwiftTests.swift
+//  TransformersAppSwiftTests
+//
+//  Created by Anjali Pragati Dennis on 06/07/20.
+//  Copyright © 2020 Anjali Pragati Dennis. All rights reserved.
+//
+
+import XCTest
+@testable import TransformersAppSwift
+
+class TransformersAppSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
